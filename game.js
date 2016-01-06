f0e9c627d34fa38f4db58589179a3f3b874ac84c
Gary Torres <Gary Torres>
2016-01-06 14:26:15 -0600

    Refactored board to use a 2D array and added simple logic to play a game by switching between players. The game is not smart enough yet to tell if the game has ended or not

--- game.js
+++ game.js
@@ -1,21 +1,56 @@
 var board = [];
+var PLAYER1 = 'x';
+var PLAYER2 = 'o';
+var EMPTYSPACE = '-';
+var currentPlayer = PLAYER1;
 
-function makeMove(player, position){
-    board[position] = player;
+function makeMove(move) {
+    var player = currentPlayer;
+    console.log('Player ' + player + ' made a move at ', move)
+    if (isMoveValid(move)) {
+        setCell(player, move);
+        if (checkWin(player, move)) {
+            alert('Player ' + currentPlayer + ' won the game!');
+        }
+        else {
+            switchCurrentPlayer();
+        }
+    }
+    else{
+        console.log('Invalid move');
+    }
+
     return board;
 }
 
-
-function checkWin(player, lastMove){
-    return true;
+function checkWin(player, lastMove) {
+    return false;
 }
 
-function isMoveValid(player, position){
-    return true;
+function setCell(value, move) {
+    board[move.x][move.y] = value;
 }
 
+function switchCurrentPlayer() {
+    currentPlayer = (currentPlayer === PLAYER1) ? PLAYER2 : PLAYER1;
+}
 
-function createNewBoard(width, height){
+function isMoveValid(move) {
+    var valid = false;
+    if (board[move.x][move.y] === EMPTYSPACE) {
+        valid = true;
+    }
+    return valid;
+}
+
+function createNewBoard(columns, rows) {
     var newBoard = [];
+
+    for (var colIndex = 0; colIndex < columns; colIndex++) {
+        newBoard[colIndex] = [];
+        for (var rowIndex = 0; rowIndex < rows; rowIndex++) {
+            newBoard[colIndex][rowIndex] = EMPTYSPACE;
+        }
+    }
     return newBoard;
 }
